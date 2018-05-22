63ed376f7376961a49a52e91062fadd8ac706d24
mihiradaiki <one-piece.p.d.ace@gmail.com>
2018-05-22 16:43:08 +0900

    Capsul

---
+++ Capsul/src/Capsul.java
@@ -0,0 +1,16 @@
+
+public class Capsul {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+
+		Person taro=new Person("山田太郎",20);
+
+		System.out.println(taro.getName());
+		System.out.println(taro.getAge());
+
+
+	}
+
+}
